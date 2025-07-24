8d89d2c79cf0acbcc64ebea425d1cd5d3f6a8221
Danna Patricia <dannapatriciacm@gmail.com>
2025-07-24 18:12:30 +0200

    Add files via upload

--- soporte-tecnico-app/src/stores/httpStore.js
+++ soporte-tecnico-app/src/stores/httpStore.js
@@ -6,7 +6,7 @@ export const useHttpStore = defineStore('http', {
   // estado donde se guarda la instancia axios
   state: () => ({
     apiClient: axios.create({
-      baseURL: 'http://soporte-tecnico-app-production.up.railway.app',
+      baseURL: 'https://soporte-tecnico-app-production.up.railway.app',
       withCredentials: false,
       headers: {
         Accept: 'application/json',
