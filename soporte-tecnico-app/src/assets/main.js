3d53b055e3d0321a64a7ea2e1c975c843a491ca4
Danna Patricia <dannapatriciacm@gmail.com>
2025-07-24 14:33:33 +0200

    Delete soporte-tecnico-app directory

--- soporte-tecnico-app/src/assets/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("toggleMenu").addEventListener("click", function () {
-    document.querySelector(".user-nav").classList.toggle("active")
-    document.querySelector(".web-nav").classList.toggle("active")
-})
