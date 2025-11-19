180b40e463ff629f3d15d8eb9aab4d8981fbf2f9
Danna Patricia Collaguazo Mera <dannapatriciacm@gmail.com>
2025-11-19 21:57:10 +0100

    Arreglo del backend

--- soporte-tecnico-app/src/stores/httpStore.js
+++ soporte-tecnico-app/src/stores/httpStore.js
@@ -6,7 +6,7 @@ export const useHttpStore = defineStore('http', {
   // estado donde se guarda la instancia axios
   state: () => ({
     apiClient: axios.create({
-      baseURL: 'https://soporte-tecnico-app-production.up.railway.app',
+      baseURL: 'https://soporte-tecnico-app.onrender.com',
       withCredentials: false,
       headers: {
         Accept: 'application/json',
